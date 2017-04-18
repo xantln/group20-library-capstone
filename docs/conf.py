a283cf44a8a0ce8035cb157ebcefe6fcf1f5cb9c
tdpearson <tylerdpearson@gmail.com>
2017-04-18 12:37:36 -0500

    update copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'cyberCommons Framework'
-copyright = u'2010, cyberCommons'
+copyright = u'2017, cyberCommons'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
