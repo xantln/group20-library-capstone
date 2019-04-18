f111bb4f8018643652b3ff2be6685901cad862f8
Mark Stacy <mbstacy@gmail.com>
2019-04-18 09:39:15 -0600

    remove URI from env file and handle creation in celery and api

--- api/config.py
+++ api/config.py
@@ -19,30 +19,34 @@ MONGO_DB = os.environ.get('MONGO_DB',"cybercom")
 MONGO_LOG_COLLECTION = os.environ.get('MONGO_LOG_COLLECTION',"task_log")
 MONGO_TOMBSTONE_COLLECTION = os.environ.get('MONGO_TOMBSTONE_COLLECTION',"tombstone")
 
-SSL_PATH=os.environ.get('SSL_PATH','{0}/docker_config/ssl/backend'.format(BASE_DIR))
-BROKER_URL = os.environ.get('BROKER_URL','amqp://quser:RJAkDsP7UkW9@cybercom_rabbitmq:5671/vhost')
+SSL_PATH=os.environ.get('SSL_PATH') 
+RABBITMQ_DEFAULT_USER=os.environ.get('RABBITMQ_DEFAULT_USER')
+RABBITMQ_DEFAULT_PASS=os.environ.get('RABBITMQ_DEFAULT_PASS')
+RABBITMQ_DEFAULT_VHOST=os.environ.get('RABBITMQ_DEFAULT_VHOST')
+BROKER_URL="amqp://{0}:{1}@cybercom_rabbitmq:5671/{2}"
+BROKER_URL = BROKER_URL.format(RABBITMQ_DEFAULT_USER,RABBITMQ_DEFAULT_PASS,RABBITMQ_DEFAULT_VHOST)
 BROKER_USE_SSL = {
     'keyfile': '{0}/client/key.pem'.format(SSL_PATH),
     'certfile': '{0}/client/cert.pem'.format(SSL_PATH),
     'ca_certs': '{0}/testca/cacert.pem'.format(SSL_PATH),
     'cert_reqs': ssl.CERT_REQUIRED
 }
-
-MONGO_URI = os.environ.get('MONGO_URI','mongodb://quser:RJAkDsP7UkW9@cybercom_mongo:27017/?ssl=true&ssl_ca_certs={0}/testca/cacert.pem&ssl_certfile={0}/client/mongodb.pem'.format(
-    SSL_PATH))
+MONGO_USERNAME=os.environ.get('MONGO_USERNAME')
+MONGO_PASSWORD=os.environ.get('MONGO_PASSWORD')
+MONGO_URI="mongodb://{0}:{1}@cybercom_mongo:27017/?ssl=true&ssl_ca_certs={2}/testca/cacert.pem&ssl_certfile={2}/client/mongodb.pem" 
+MONGO_URI= MONGO_URI.format(MONGO_USERNAME,MONGO_PASSWORD,SSL_PATH)
 CELERY_RESULT_BACKEND = MONGO_URI
 CELERY_MONGODB_BACKEND_SETTINGS = {
     "database": MONGO_DB,
     "taskmeta_collection": MONGO_TOMBSTONE_COLLECTION
 }
-
 # ******* Catalog ******************************************************
-CATALOG_EXCLUDE = ['admin','config', 'local', MONGO_DB]
+CATALOG_EXCLUDE = ['admin','config','local','default_collection',MONGO_DB]
 CATALOG_INCLUDE = ['catalog']
 CATALOG_URI = MONGO_URI
 CATALOG_ANONYMOUS = True
 # *********** Data Store ************************************************
-DATA_STORE_EXCLUDE = ['admin','config', 'local', MONGO_DB,'catalog']
+DATA_STORE_EXCLUDE = ['admin','config','local','default_collection',MONGO_DB,'catalog']
 DATA_STORE_MONGO_URI = MONGO_URI
 DATA_STORE_ANONYMOUS = True
 
--- dc_config/images/celery/celeryconfig.py
+++ dc_config/images/celery/celeryconfig.py
@@ -10,12 +10,24 @@ def setBrookerSSL():
     else:
         return None
 
-BROKER_URL = os.environ.get('BROKER_URL')
+# SETUP BROOKER URI
+RABBITMQ_DEFAULT_USER=os.environ.get('RABBITMQ_DEFAULT_USER')
+RABBITMQ_DEFAULT_PASS=os.environ.get('RABBITMQ_DEFAULT_PASS')
+RABBITMQ_DEFAULT_VHOST=os.environ.get('RABBITMQ_DEFAULT_VHOST')
+BROKER_URL="amqp://{0}:{1}@cybercom_rabbitmq:5671/{2}"
+BROKER_URL = BROKER_URL.format(RABBITMQ_DEFAULT_USER,RABBITMQ_DEFAULT_PASS,RABBITMQ_DEFAULT_VHOST)
 BROKER_USE_SSL = setBrookerSSL()
 CELERY_SEND_EVENTS = True
 CELERY_TASK_RESULT_EXPIRES = None
 CELERY_ACCEPT_CONTENT = ['json']
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND') 
+
+# SETUP MONGO URI
+SSL_PATH=os.environ.get('SSL_PATH') 
+MONGO_USERNAME=os.environ.get('MONGO_USERNAME')
+MONGO_PASSWORD=os.environ.get('MONGO_PASSWORD')
+MONGO_URI="mongodb://{0}:{1}@cybercom_mongo:27017/?ssl=true&ssl_ca_certs={2}/testca/cacert.pem&ssl_certfile={2}/client/mongodb.pem" 
+CELERY_RESULT_BACKEND= MONGO_URI.format(MONGO_USERNAME,MONGO_PASSWORD,SSL_PATH)
+
 CELERY_MONGODB_BACKEND_SETTINGS = {
     "database": os.environ.get('MONGO_DB',"cybercom"),
     "taskmeta_collection": os.environ.get('MONGO_TOMBSTONE_COLLECTION',"tombstone")
