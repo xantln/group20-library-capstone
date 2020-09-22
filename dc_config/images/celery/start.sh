34008b0d55863350378a55362118fb1fa1ea6190
Tyler Pearson <tdpearson@ou.edu>
2020-09-22 17:22:54 -0500

    call pip install from module

--- dc_config/images/celery/start.sh
+++ dc_config/images/celery/start.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 echo "Mounting s3fs to /mnt/"
-
 /usr/bin/s3fs -f -o allow_other,umask=0077,mp_umask=0077,uid=1000,gid=1000 ${AWSBUCKETNAME} /mnt/ &
 
+echo "Starting Celery worker"
 /app/startCeleryWorker
 
- 
-
-
-
-echo "I have started the celery worker"
