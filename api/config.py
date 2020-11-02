b5d2788703d7ce6d46c59fa79e27bb1337e48693
Tyler Pearson <tdpearson@ou.edu>
2020-11-02 10:53:12 -0600

    update TLS config check for email

--- api/config.py
+++ api/config.py
@@ -58,5 +58,5 @@ if os.getenv('EMAIL_HOST'):
     EMAIL_PORT = os.getenv('EMAIL_PORT')
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') in ['TRUE', 'True', 'true']:
+    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'false').lower() == 'true'
 
